cec9081b67f1cf7ed0571de9d56cb21d88759f87
Rehab <rehab123abdallah@gmail.com>
2024-09-16 19:53:35 +0300

    CreationOfSession01

---
+++ Company.DAL/Contexs/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Company.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.DAL.Contexs
+{
+    public class AppDbContext :DbContext
+
+    {
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=. ;Database=CompanyMVC;Trusted_Connection =True");
+        //}
+        //public DbSet<Department> DepartmentSet { get; set; }
+    }
+}
