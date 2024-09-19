7d3419dcfd5e2bdf21bcaabe424a4d279d0e713d
Rehab <rehab123abdallah@gmail.com>
2024-09-19 12:18:47 +0300

    Session01(Department Controller Details)

--- Company.PL/Controllers/DepartmentController.cs
+++ Company.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,6 @@
 ﻿using Company.BLL.Interfaces;
 using Company.BLL.Repositries;
+using Company.DAL.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.PL.Controllers
@@ -17,5 +18,40 @@ namespace Company.PL.Controllers
             var department = _departmentRepositry.GetAll();
             return View(department);
         }
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                _departmentRepositry.Add(department);
+                return RedirectToAction(nameof(Index));
+
+            }
+
+            return View();
+        }
+        public IActionResult Details(int? id) {
+            if(id == null)
+            {
+                return BadRequest();
+            }
+            var department = _departmentRepositry.GetById(id.Value);
+            if (department == null) {
+                return BadRequest();
+            }
+            else
+            {
+                return View(department);
+            }
+
+        }
+
+
     }
 }
